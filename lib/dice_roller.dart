468c112197af983a56d68993eea2dc52d3d78f30
Phan Nguyen <phannguyen@swissmarketplace.group>
2024-05-19 11:16:42 +0700

    add random

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+final randomizer = Random();
 
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
@@ -12,14 +15,12 @@ class DiceRoller extends StatefulWidget {
 
 // underscore make this class be private use in this file
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = 'assets/images/dice-1.png';
-
+  var currentDiceRoll = 1;
   // this action requires to use a stateful widget to change the UI state of the widget
   void rollDice() {
     // update state of a stateful widget
     setState(() {
-      activeDiceImage = 'assets/images/dice-3.png';
-      // print("Change image $activeDiceImage");
+      currentDiceRoll = randomizer.nextInt(6) + 1;
     });
   }
 
@@ -30,7 +31,7 @@ class _DiceRollerState extends State<DiceRoller> {
         mainAxisSize: MainAxisSize.min, // center vertical
         children: [
           Image.asset(
-            activeDiceImage,
+            'assets/images/dice-$currentDiceRoll.png',
             width: 200,
           ),
           const SizedBox(
